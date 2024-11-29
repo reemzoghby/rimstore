b116e2069a167567f6ed67266dae9eb6cec25d89
Assaad El Najjar <assaad.el-najjar@konplan.cz>
2024-11-29 19:58:43 +0100

    add cors

--- rimstore_api/index.js
+++ rimstore_api/index.js
@@ -1,10 +1,12 @@
 require("./config/dotenv");
 
 const express = require("express");
+const cors = require('cors')
 const { sequelize } = require("./models");
 const routes = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 
